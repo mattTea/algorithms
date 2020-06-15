927825e9501e72f79b253a9f901e3221059c39c2
Matt Thompson <matt.thompson@johnlewis.co.uk>
2020-06-15 21:41:28 +0100

    Randomise pivot in quickSort

--- src/main/kotlin/algorithms/QuickSort.kt
+++ src/main/kotlin/algorithms/QuickSort.kt
@@ -4,8 +4,9 @@ fun quickSort(list: List<Int>): List<Int> {
     return if (list.size < 2) {
         list
     } else {
-        val pivot = list.first()
-        val lessThanPivot = list.drop(1).filter { it <= pivot }
+        val randomIndex = list.indices.random()
+        val pivot = list[randomIndex]
+        val lessThanPivot = (list - list[randomIndex]).filter { it <= pivot }
         val moreThanPivot = list.filter { it > pivot }
 
         quickSort(lessThanPivot) + pivot + quickSort(moreThanPivot)
