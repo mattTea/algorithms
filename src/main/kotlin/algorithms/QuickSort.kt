f83807854befa111c12d3c0cfdfc26aa04b2e7dd
Matt Thompson <matt.thompson@johnlewis.co.uk>
2020-06-15 21:27:12 +0100

    Simplify quickSort to base and recursive case

--- src/main/kotlin/algorithms/QuickSort.kt
+++ src/main/kotlin/algorithms/QuickSort.kt
@@ -1,15 +1,13 @@
 package algorithms
 
 fun quickSort(list: List<Int>): List<Int> {
-    return when {
-        list.size < 2 -> list
-        list.size == 2 -> { if (list[0] < list[1]) list else list.reversed() }
-        else -> {
-            val pivot = list.first()
-            val lessThanPivot = list.drop(1).filter { it <= pivot }
-            val moreThanPivot = list.filter { it > pivot }
+    return if (list.size < 2) {
+        list
+    } else {
+        val pivot = list.first()
+        val lessThanPivot = list.drop(1).filter { it <= pivot }
+        val moreThanPivot = list.filter { it > pivot }
 
-            quickSort(lessThanPivot) + pivot + quickSort(moreThanPivot)
-        }
+        quickSort(lessThanPivot) + pivot + quickSort(moreThanPivot)
     }
 }
